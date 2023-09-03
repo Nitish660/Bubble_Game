833b0e3ed90e62c3d79afe6e356776a77c7a31f3
Ntish660 <20051742@kiit.ac.in>
2023-09-03 11:02:56 +0530

    commit

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
+var timer = 60;
+var score = 0;
+var hitrn;
 
-
-function MakeBubble(){
-    var clutter = "";
-
-    for(var i = 1; i < 190; i++) {
-        var rn = Math.floor(Math.random() * 10); // Corrected Math.random()
-        clutter += `<div class="bubble">${rn}</div>`;
-    }
-    
-    document.querySelector("#pbtm").innerHTML = clutter;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
 }
 
 
+function MakeBubble() {
+    var clutter = "";
+
+    for (var i = 1; i < 190; i++) {
+        var rn = Math.floor(Math.random() * 10); // Corrected Math.random()
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+   var timerint = setInterval(function(){
+      if(timer>0)
+      {
+        timer --;
+        document.querySelector("#timerval").textContent=timer;
+      }
+      else{
+          clearInterval(timerint);
+          document.querySelector("#pbmt")=`<h2>Game Over>`
+      }
+    },1000);
+}
+
+function getNewHit(){
+       hitrn = Number(Math.floor(Math.random()*10));
+       document.querySelector("#hitval").textContent=hitrn;
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click",function(dets){
+     
+
+})
+
+
 MakeBubble();
+runTimer();
+getNewHit();
+
 
